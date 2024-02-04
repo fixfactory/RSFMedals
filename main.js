a350dc4965b4ac6a726c1715c113b47383936294
fixfactory <5952846+fixfactory@users.noreply.github.com>
2024-02-04 09:52:00 -0800

    Updated target times formatting. Added comments.

--- main.js
+++ main.js
@@ -18,6 +18,7 @@
 // with RSF Medals. If not, see <http://www.gnu.org/licenses/>.
 //
 
+// Takes a time string in the format "6:45.123" and returns a Date object.
 function parseTime(timeString) {
   let colon = timeString.indexOf(':')
   let dot = timeString.indexOf('.')
@@ -27,17 +28,21 @@ function parseTime(timeString) {
   return new Date(0, 0, 0, 0, minutes, seconds, milliseconds)
 }
 
+// Returns the total number of milliseconds in a Date object.
 function totalMs(date) {
   return date.getMinutes() * 60 * 1000 +
     date.getSeconds() * 1000 +
     date.getMilliseconds()
 }
 
+// Pads a number with leading zeros.
+// Example: pad(3, 2) returns "03"
 function pad(num, size) {
   var s = "000" + num;
   return s.substr(s.length - size);
 }
 
+// Returns the target time string relative to a record time.
 function targetTime(relative, record) {
   let recordMs = totalMs(record)
   let targetMs = recordMs * relative / 100
@@ -47,17 +52,20 @@ function targetTime(relative, record) {
     pad(target.getMilliseconds(), 3)
 }
 
+// Parses a row of the record times table and inject HTML.
 function parseRow(row) {
   let cells = row.querySelectorAll("td");
   if (cells.length < 8) {
     return
   }
 
+  // Personal record cell
   let timeDiv = cells[6].querySelectorAll("div");
   if (timeDiv.length <= 0 || !timeDiv[0].firstChild) {
     return
   }
 
+  // World record cell
   let recordDiv = cells[7].querySelectorAll("div");
   if (recordDiv.length <= 0 || !recordDiv[0].firstChild) {
     return
@@ -67,11 +75,13 @@ function parseRow(row) {
   let record = parseTime(String(recordDiv[0].firstChild.data))
   let relative = totalMs(time) / totalMs(record) * 100
 
+  // Constants used to determine the target times
   const wr = 100
   const gold = 105
   const silver = 110
   const bronze = 120
 
+  // Inject the medal emoji
   if (relative <= wr) {
     timeDiv[0].firstChild.data = "🏆 " + timeDiv[0].firstChild.data
   } else if (relative <= gold) {
@@ -84,12 +94,13 @@ function parseRow(row) {
     //timeDiv[0].firstChild.data = "💩 " + timeDiv[0].firstChild.data
   }
 
+  // Inject the target times in the mouse over tooltip
   let targetTimes = "<br><br>" +
-    "🥇 +" + String(gold - 100) + "% " + targetTime(gold, record) +
+    "🥇 " + targetTime(gold, record) + " (+" + String(gold - 100) + "%)" +
     "<br>" +
-    "🥈 +" + String(silver - 100) + "% " + targetTime(silver, record) +
+    "🥈 " + targetTime(silver, record) + " (+" + String(silver - 100) + "%)" +
     "<br>" +
-    "🥉 +" + String(bronze - 100) + "% " + targetTime(bronze, record) +
+    "🥉 " + targetTime(bronze, record) + " (+" + String(bronze - 100) + "%)" +
     "<br>"
 
   let mouseover = timeDiv[0].getAttribute("onmouseover")
@@ -98,11 +109,13 @@ function parseRow(row) {
   timeDiv[0].setAttribute("onmouseover", mouseover)
 }
 
+// Parse all the odd rows.
 var rows = document.getElementsByClassName("paratlan")
 for (let i = 0; i < rows.length; i++) {
   parseRow(rows[i]);
 }
 
+// Parse all the even rows.
 rows = document.getElementsByClassName("paros")
 for (let i = 0; i < rows.length; i++) {
   parseRow(rows[i]);
